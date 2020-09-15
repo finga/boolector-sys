20e7265f398c52af3655428bc603f5e76b73d2c1
finga <finga@onders.org>
2020-09-15 16:48:09 +0200

    Implement use of `boolector-src`
    In `Cargo.toml` add build dependencies for `boolector`, cargo feature
    for `boolector-src` and that it links boolector. Also adapt `build.rs`
    to be able to build `boolector-src`.

--- build.rs
+++ build.rs
@@ -1,3 +1,22 @@
 fn main() {
+    #[cfg(feature = "vendored")]
+    {
+        use std::env;
+
+        // vendor if the feature is present, unless
+        // BOOLECTOR_NO_VENDOR exists and isn't `0`
+        if env::var("BOOLECTOR_NO_VENDOR").map_or(true, |s| s == "0") {
+            let boolector = boolector_src::Build::new().prerequisites().build();
+            println!("cargo:vendored=1");
+            println!(
+                "cargo:root={}",
+                boolector.lib_dir().parent().unwrap().display()
+            );
+            boolector.print_cargo_metadata();
+
+            ()
+        }
+    }
+
     println!("cargo:rustc-link-lib=boolector");
 }
